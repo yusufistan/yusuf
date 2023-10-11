7a250f9b66bb9369f7733ef02412412e7f4790c8
OguzKoran <ouz_x3@hotmail.com>
2023-10-11 12:43:47 +0300

    Add project files.

---
+++ yusuf/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace yusuf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("merhaba gardaş");
+        }
+    }
+}
